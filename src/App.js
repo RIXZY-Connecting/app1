33cd66dfbd31126f22066f80c9dbc5c62ed98d90
RIXZY <152828921+LetRIXZY@users.noreply.github.com>
2024-01-08 10:54:27 +0700

    update func-components add content

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import './style.css';
 import React from 'react';
-import Header from './func-components.js';
+import {Content,Header }from './func-components.js';
 
 function App() {
     return (
-        <>
-            <h2>DeverlopThai.com</h2>
-            <div className='title'>React & React Native</div>
-        </>
+       [<Header/>,<Content/>]
     )
 }   
 
--- src/func-components.js
+++ src/func-components.js
@@ -9,9 +9,17 @@ export function Header(){
  }
  return(
     <div style={{backgroundColor:'#ccc'}}>
-    <a href="" style={aStyle}Home></a>|&nbsp
-    <a href="" style={aStyle}>React</a>|&nbsp
+    <a href="" style={aStyle}>Home</a>|&nbsp;
+    <a href="" style={aStyle}>React</a>|&nbsp;
     <a href="" style={aStyle}>React Native</a>
     </div>
  )
  }
+export function Content(){
+    return(
+        <div style={{textAlign:'center'}}>
+            <h2>Hello <br/>React & React Native</h2>
+            <br/><br/><br/>
+        </div>
+    )
+}
