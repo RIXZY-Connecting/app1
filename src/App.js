dc90658fd899f187301cf7b5f7b2ce15bcbca7c1
RIXZY <152828921+LetRIXZY@users.noreply.github.com>
2024-02-19 09:53:44 +0700

    create form-bootstrap.js
    create form-bootstrap.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import FormSearch from './form-search';
+import FormBootstrap from './form-bootstrap';
 
 export default function App() {
-    return <FormSearch/>
+    return <FormBootstrap/>
 }   
